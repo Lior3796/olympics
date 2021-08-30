a18acb67c598a3bf4bdc60b3db13f4eb8ac4276e
Lior3796 <liorlsa9@gmail.com>
2021-08-30 21:03:47 +0300

    add gh-pages deploy dcripts

--- src/AppRouter/AppRouter.jsx
+++ src/AppRouter/AppRouter.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Home from '../Features/Pages/Home/Home';
 import Content from '../Features/Pages/Content/Content';
-import { AnimatePresence,motion } from 'framer-motion';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { AnimatePresence, motion } from 'framer-motion';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import OlympicsData from '../Features/Pages/OlympicsData/OlympicsData';
 import Registration from '../Features/Components/Form/registration';
 import AthletesCards from '../Features/Components/AthletesCards/AthletesCards';
@@ -10,22 +10,22 @@ import { FormRegister } from '../Features/Components/FormRegister/FormRegister';
 import NavBar from '../Features/Components/NavBar/NavBar';
 import Footer from '../Features/Components/Footer/Footer';
 
-function AppRouter() {    
-   const variant = {
-       hidden:{
-           opacity:0
-       },
-       visible:{
-        opacity:1
+function AppRouter() {
+    const variant = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1
+        }
     }
-   }
     return (
         <Router>
             <NavBar position />
-            <NavBar  />   
+            <NavBar />
             <Switch >
                 <Route path="/" exact>
-                    <Home  />
+                    <Home />
                     <Content />
                     <OlympicsData />
                 </Route>
@@ -38,9 +38,10 @@ function AppRouter() {
                     <FormRegister />
                 </Route>
                 <Footer />
+                <Redirect to="/" />
             </Switch>
 
-        <Footer />
+            <Footer />
         </Router>
     );
 }
